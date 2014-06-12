454a561199b880fdd3c9f7f37ef96d473cb4c1ca
Muhammad Naviwala <muhammad.naviwala@parivedasolutions.com>
2014-06-12 16:14:10 -0500

    mocking TI network calls

--- app/spec/data_retriever_spec.js
+++ app/spec/data_retriever_spec.js
@@ -7,11 +7,26 @@ describe("Testing parsedJson", function() {
 		var dataToSend = "\"social-media-settings\": {\"liking\": true,\"sharing\": false,\"commenting\": true},";
 		var expectedData = "\"social-media-settings\": {\"liking\":expectedData true,\"sharing\": false,\"commenting\": true},";
 		spyOn(tiCalls, 'parse').andReturn(expectedData);
-		
+
 		// Act
 		var returnedData = dataRetriever.parseJson(dataToSend);
-		
+
 		// Assert
 		expect(returnedData).toBe(expectedData);
+	});
+});
+
+describe("Testing network", function() {
+	it("should return valid http client", function() {
+		// Arrange
+		var url = "http://api.openweathermap.org/data/2.5/weather?q=Housotn,us&mode=json&units=imperial";
+		spyOn(tiCalls, 'network').andReturn();
+
+		// Act
+		var returnedData = dataRetriever.fetchDataFromUrl(url);
+
+		// Assert
+		// expect(returnedData).not.toBeNull();
+		console.log(returnedData);
 	});
 });
