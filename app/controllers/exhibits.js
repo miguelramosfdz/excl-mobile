a3def9fca1e11e7732bad9a73d4f7368b1529efd
Kyle Clark <kyle.clark@parivedasolutions.com>
2014-06-16 08:14:01 -0500

    no message

--- app/controllers/exhibits.js
+++ app/controllers/exhibits.js
@@ -1,16 +1,54 @@
 var args = arguments[0] || {};
 
+var json = {
+  "status": "ok",
+  "error": "Optional Error Message",
+  "data": {
+    "museum": {
+      "name": "Children's Museum of Houston",
+      "about": {
+        "description": "This is the museum description here.",
+        "map": "http://2.bp.blogspot.com/-Aa7MNnxjsqc/UAaxfj35ohI/AAAAAAAANBo/VVfnQt399rQ/s1600/Dolphin.jpg",
+        "prices": "$5/person",
+        "image": "http://2.bp.blogspot.com/-Aa7MNnxjsqc/UAaxfj35ohI/AAAAAAAANBo/VVfnQt399rQ/s1600/Dolphin.jpg"
+      },
+      "contact": {
+        "website": "http://cmh.org",
+        "phone": "878787878",
+        "email": "me@cmh.org"
+      },
+      "exhibits": [
+        {
+          "id": 12345,
+          "name": "How Does It Work?",
+          "description": "Such a great exhibit. Check it out!",
+          "image": "http://2.bp.blogspot.com/-Aa7MNnxjsqc/UAaxfj35ohI/AAAAAAAANBo/VVfnQt399rQ/s1600/Dolphin.jpg",
+          "components": [
+            {
+              "id": 12345,
+              "name": "Cups and Balls",
+              "image": "http://2.bp.blogspot.com/-Aa7MNnxjsqc/UAaxfj35ohI/AAAAAAAANBo/VVfnQt399rQ/s1600/Dolphin.jpg"
+            },
+            {
+              "id": 54321,
+              "name": "Spinning Disc",
+              "image": "http://2.bp.blogspot.com/-Aa7MNnxjsqc/UAaxfj35ohI/AAAAAAAANBo/VVfnQt399rQ/s1600/Dolphin.jpg"
+            }
+          ]
+        }
+      ]
+    }
+  }
+};
+
 var tableData = [];
-var exhibitsImages = ['http://placehold.it/700x300/556270', 'http://placehold.it/700x200/4ECDC4', 'http://placehold.it/600x300/C7F464', 'http://placehold.it/600x200/FF6B6B', 'http://placehold.it/700x300/C44D58'];
-
-
 // simulate data from wordpress using Jess' model
 
 // Modify with new MODULE methods
 function openComponent(e){
 		
 	var componentWindow = Alloy.createController('componentlanding').getView();
-	componentWindow.open();  
+	//componentWindow.open();  
 }
 
 function openExhibitInformation(e){
@@ -42,7 +80,7 @@ function createHeadingRow() {
 
 
 
-function createExhibitsCarousel(headingText, imageCollection){
+function createExhibitsCarousel(headingText, exhibits){
 	
 	var row = createPlainRow();
 	var headingRow = createHeadingRow();
@@ -62,19 +100,44 @@ function createExhibitsCarousel(headingText, imageCollection){
 
 	var imageWrappers = [];
 	
-	for (var i = 0; i < 5; i++){//imageCollection.length; i++) {
+	for (var i = 0; i < exhibits.length; i++){//imageCollection.length; i++) {
 		var tempImage = Ti.UI.createImageView({
-			image : imageCollection[i],
+			image : exhibits[i].image,
 			defaultImage : 'http://placehold.it/700x300',
 			width : '100%',
 			height : '100%'
 		});
-		var tempWrapper = Ti.UI.createScrollView({
-			maxZoomScale : 4.0,
+		var tempWrapper = Ti.UI.createView({
+			width : '100%',
+			height : '100%',
+			backgroundColor: 'white',
+			opacity: '50%',
 		});
+		
+		var labelTint = Ti.UI.createView({
+			backgroundColor: 'black',
+			opacity: 0.5,
+			height: '20%',
+			top: 0
+		});
+		
+		var label = Ti.UI.createLabel({
+			text: exhibits[i].name,
+			top: 0,
+			left: 10,
+			color: 'white',
+			font: {
+				fontFamily : 'Arial',
+				fontSize : 22,
+				fontWeight : 'bold'
+			}
+		});
+		
+		labelTint.add(label);
 		tempWrapper.add(tempImage);
-		imageWrappers[i] = tempImage;
+		tempWrapper.add(labelTint);
 		//imageWrappers[i] = tempImage;
+		imageWrappers[i] = tempWrapper;
 	}
 
 	var scrollableView = Ti.UI.createScrollableView({
@@ -121,17 +184,45 @@ function createComponentsScrollView(componentsMessage, components){
 		horizontalWrap: false
 	});
 	
-	for (var i = 0; i < 5; i++){//imageCollection.length; i++) {
+	for (var i = 0; i < components.length; i++){//imageCollection.length; i++) {
 		image = Ti.UI.createImageView({
-			image : components[i],
+			image : components[i].image,
 			defaultImage : 'http://placehold.it/700x300',
-			left: 10,
-			right: 10,
 			height: '100%',
 			width: 200
+		});				
+		
+		var wrapper = Ti.UI.createView({
+			backgroundColor: 'white',
+			width: 200,
+			left: 10,
+			right: 10
 		});
+		var labelTint = Ti.UI.createView({
+			backgroundColor: 'black',
+			opacity: 0.5,
+			height: '20%',
+			top: 0
+		});
+		var label = Ti.UI.createLabel({
+			text: components[i].name,
+			top: 0,
+			left: 10,
+			color: 'white',
+			font: {
+				fontFamily : 'Arial',
+				fontSize : 14,
+				fontWeight : 'bold'
+			}
+		});	
+		
 		image.addEventListener('click', openComponent);
-		scrollView.add(image);
+		
+		labelTint.add(label);
+		wrapper.add(image);
+		wrapper.add(labelTint);
+		
+		scrollView.add(wrapper);
 	}
 	
 	row.add(scrollView);
@@ -154,14 +245,14 @@ function createExhibitText(text){
 	});
 	textRow.top = 20;
 	textRow.add(label);
-	textRow.height = 150;
+	textRow.height = 120;
 	tableData.push(textRow);
 }
 
 
 
-createExhibitsCarousel("All Exhibits", exhibitsImages);
-createComponentsScrollView("Check out our Stations", exhibitsImages);
+createExhibitsCarousel("All Exhibits", json.data.museum.exhibits);
+createComponentsScrollView("Check out our Stations", json.data.museum.exhibits[0].components);
 createExhibitText("Blah blah blah blah blah blah blah blah,\nBlah blah blah.");
 
 var tableView = Ti.UI.createTableView({
