a90fd5eebc86117021f8f32c41f26ef55681f6e9
Alex Pearce <alex.pearce@parivedasolutions.com>
2014-06-16 15:25:21 -0500

    removed useless images and repaired formatting

--- app/controllers/social.js
+++ app/controllers/social.js
@@ -1,6 +1,26 @@
 //top level vars
 var imageFilePath;
 
+function formatButtonIOS(buttonName) {
+	//Format buttons for IOS
+	if (OS_IOS) {
+		//remove title field if image is present
+		if (buttonName.backgroundImage != "") {
+			buttonName.title = "";
+		}
+	}
+}
+
+function formatButtonAndroid(buttonName) {
+	//format buttons for Android
+	if (OS_ANDROID) {
+		//remove title field if image is present
+		if (buttonName.backgroundImage != "") {
+			buttonName.title = "";
+		}
+	}
+}
+
 function createShareButtons() {
 	//create view that will serve as temporary backing for sharing buttons
 	var viewSharingTemp = Ti.UI.createView({
@@ -22,6 +42,8 @@ function createShareButtons() {
 	shareText.addEventListener('click', function(e) {
 		sendIntentText();
 	});
+	formatButtonIOS(shareText);
+	formatButtonAndroid(shareText);
 	viewSharingTemp.add(shareText);
 
 	//button to open photo sharing
@@ -36,6 +58,8 @@ function createShareButtons() {
 	shareImage.addEventListener('click', function(e) {
 		openCamera();
 	});
+	formatButtonIOS(shareImage);
+	formatButtonAndroid(shareImage);
 	viewSharingTemp.add(shareImage);
 }
 
@@ -54,7 +78,7 @@ function openCamera() {
 			var imageFile = Ti.Filesystem.getFile('file:///sdcard/').exists() ? Ti.Filesystem.getFile('file:///sdcard/', fileName) : Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, fileName);
 			imageFile.write(event.media);
 			//save file path to be shared
-			
+
 			if (event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
 				imageFilePath = imageFile.nativePath;
 				sendIntentImage();
@@ -70,12 +94,12 @@ function openCamera() {
 
 function sendIntentImage() {
 	//create and send an image intent
-	
+
 	//Get text to be sent from WP
 	contentTextComment = "#cmh #awesome";
 	contentTextSubject = "Having fun at Children's Museum of Houston!";
 	contentTextURL = "http://www.cmhouston.org";
-	
+
 	if (OS_ANDROID) {
 		sendIntentImageAndroid(contentTextComment, contentTextSubject, contentTextURL);
 	} else if (OS_IOS) {
@@ -86,16 +110,17 @@ function sendIntentImage() {
 }
 
 function sendIntentImageAndroid(contentTextComment, contentTextSubject, contentTextURL) {
-	contentTextComment = contentTextComment + contentTextURL; //Android intents don't have a separate URL field
+	contentTextComment = contentTextComment + contentTextURL;
+	//Android intents don't have a separate URL field
 
-	//Create and send image intent for android. 
+	//Create and send image intent for android.
 	var intentImage = Ti.Android.createIntent({
 		type : "image/*",
 		action : Ti.Android.ACTION_SEND
 	});
 	intentImage.addCategory(Ti.Android.CATEGORY_DEFAULT);
 	intentImage.putExtraUri(Ti.Android.EXTRA_STREAM, imageFilePath);
-	Ti.Android.currentActivity.startActivity(Ti.Android.createIntentChooser(intentImage, "Share with..."));
+	Ti.Android.currentActivity.startActivity(Ti.Android.createIntentChooser(intentImage, "Share photo via"));
 }
 
 function sendIntentImageiOS(contentTextComment, contentTextSubject, contentTextURL) {
@@ -136,13 +161,12 @@ function sendIntentTextAndroid(contentTextComment, contentTextSubject, contentTe
 		action : Ti.Android.ACTION_SEND,
 		type : 'text/plain'
 	});
-	contentTextComment = contentTextComment + "\n"+ contentTextURL;
+	contentTextComment = contentTextComment + "\n" + contentTextURL;
 	//Android doesn't have a separate URL field
 	intentText.putExtra(Ti.Android.EXTRA_SUBJECT, contentTextSubject);
 	intentText.putExtra(Ti.Android.EXTRA_TEXT, contentTextComment);
 	intentText.addCategory(Ti.Android.CATEGORY_DEFAULT);
-	Ti.Android.createIntentChooser(intentText, "Send Message");
-	Ti.Android.currentActivity.startActivity(intentText);
+	Ti.Android.currentActivity.startActivity(Ti.Android.createIntentChooser(intentText, "Send message via"));
 }
 
 function sendIntentTextiOS(contentTextComment, contentTextSubject, contentTextURL) {
