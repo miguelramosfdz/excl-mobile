eb6a53bbb5d55c6060d7e34aa2af0f8873210e86
JenniferLong <jennifer.long@parivedasolutions.com>
2014-06-18 14:36:21 -0500

    Cleaned up Rozay page

--- app/lib/sharing/sharing.js
+++ app/lib/sharing/sharing.js
@@ -40,7 +40,7 @@ function createTextShareButton(json) {
 		height : "40dip",
 		width : "40dip",
 		left : "0",
-		top : "0"
+		top : "300dip"
 	});
 	toggleTextShareButtonStatusInactive(shareTextButton);
 
@@ -68,7 +68,7 @@ function createImageShareButton(json) {
 		height : "40dip",
 		width : "40dip",
 		left : "70dip",
-		top : "0"
+		top : "300dip"
 	});
 	toggleImageShareButtonStatusInactive(shareImageButton);
 	//Add a listener so that when clicked, openCamera is called
