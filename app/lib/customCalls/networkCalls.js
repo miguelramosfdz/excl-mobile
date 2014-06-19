ce13dade260891aae2c0d422f41b35d00e013f76
Muhammad Naviwala <muhammad.naviwala@parivedasolutions.com>
2014-06-19 17:16:24 -0500

    made nav bar prettier

--- app/lib/customCalls/networkCalls.js
+++ app/lib/customCalls/networkCalls.js
@@ -21,7 +21,7 @@ var networkCalls = {
 			},
 			onerror : function() {
 				// apiCalls.debug(e.error);
-				alert("Could not retrieve any data :()");
+				alert("Could not retrieve any data :(");
 			}
 		});
 
