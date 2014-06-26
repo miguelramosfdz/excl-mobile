c913360c736c6de176e305e792665207bca08d1e
Muhammad Naviwala <muhammad.naviwala@parivedasolutions.com>
2014-06-26 16:33:06 -0500

    added ability to add comments on the post landing page

--- app/controllers/postlanding.js
+++ app/controllers/postlanding.js
@@ -14,8 +14,8 @@ function setPathForLibDirectory(retrieveNetworkSharingLib) {
 	}
 }
 
-//Google Analytics 
-function trackPostScreen(){
+//Google Analytics
+function trackPostScreen() {
 	Alloy.Globals.analyticsController.trackScreen("Post Landing");
 }
 
@@ -44,13 +44,11 @@ function changeTitleOfThePage(name) {
  */
 function displaySocialMediaButtons(json) {
 
-	
 	// //Create anchor for instagram viewer
 	// var rightNavButton = Ti.UI.createButton({
-		// title:''
+	// title:''
 	// });
 	// $.postlanding.add(rightNavButton);
-	
 
 	var row = createPlainRow('auto');
 	if (json.text_sharing && !Alloy.Globals.navController.kioskMode) {
@@ -83,50 +81,50 @@ function displayImages(imageURL) {
 }
 
 function displayVideo(thumbnail, videoUrl) {
-	if (OS_ANDROID){
+	if (OS_ANDROID) {
 		displayVideoAndroid(thumbnail, videoUrl);
 	}
-	if (OS_IOS){
+	if (OS_IOS) {
 		displayVideoiOS(videoUrl);
 	}
 }
 
-function displayVideoAndroid(thumbnail, videoUrl){
+function displayVideoAndroid(thumbnail, videoUrl) {
 	var row = createPlainRow('200dip');
-	
+
 	//Thumbnail for image
 	thumbnailView = Ti.UI.createView({	});
 	addThumbnailImage(thumbnail, thumbnailView);
 	addPlayTriangle(thumbnailView);
 	row.add(thumbnailView);
 	tableData.push(row);
-	
+
 	//Add event listener- when thumbnail is clicked, open fullscreen video
-	thumbnailView.addEventListener('click', function(e){
+	thumbnailView.addEventListener('click', function(e) {
 		var video = Titanium.Media.createVideoPlayer({
 			url : videoUrl,
 			fullscreen : true,
 			autoplay : true
-		});	
-		
+		});
+
 		doneButton = Ti.UI.createButton({
 			title : "Done",
 			top : "0dip",
 			height : "40dip",
 			left : "10dip",
 		});
-		
-		doneButton.addEventListener('click', function(e){
+
+		doneButton.addEventListener('click', function(e) {
 			video.hide();
-	        video.release();
-	        video = null;
+			video.release();
+			video = null;
 		});
 		video.add(doneButton);
-		
+
 	});
 }
 
-function addThumbnailImage(thumbnail, thumbnailView){
+function addThumbnailImage(thumbnail, thumbnailView) {
 	var thumbnailImageView = Ti.UI.createImageView({
 		image : thumbnail,
 		width : '100%',
@@ -135,14 +133,14 @@ function addThumbnailImage(thumbnail, thumbnailView){
 	thumbnailView.add(thumbnailImageView);
 }
 
-function addPlayTriangle(thumbnailView){
+function addPlayTriangle(thumbnailView) {
 	var playTriangle = Ti.UI.createImageView({
 		image : "/images/icons_android/Video-Player-icon-small.png",
 	});
 	thumbnailView.add(playTriangle);
 }
 
-function displayVideoiOS(videoUrl){
+function displayVideoiOS(videoUrl) {
 	var row = createPlainRow('200dip');
 	var video = Titanium.Media.createVideoPlayer({
 		url : videoUrl,
@@ -174,16 +172,141 @@ function displayText(textContent) {
 
 function addTableDataToTheView() {
 	$.tableView.height = 'auto';
-	if (OS_IOS){
+	if (OS_IOS) {
 		//Accounts for bounce buffer
 		$.tableView.bottom = "48dip";
 	}
 	$.tableView.data = tableData;
 }
 
+function creatingCommentTextHeading() {
+	var row = createPlainRow('auto');
+	var commentHeading = Ti.UI.createLabel({
+		top : 20,
+		width : '94%',
+		right : '3%',
+		left : '3%',
+		color : '#232226',
+		font : {
+			fontFamily : 'Helvetica Neue',
+			fontSize : '16dp',
+			fontWeight : 'bold',
+		},
+		text : "Comments",
+		textAlign : 'center',
+		borderWidth : '1',
+		borderColor : '#aaa',
+	});
+	row.add(commentHeading);
+	tableData.push(row);
+}
+
+function addCommentToView(commentText, commentDate) {
+	var row = createPlainRow('auto');
+	var text = Ti.UI.createLabel({
+		top : 10,
+		width : '94%',
+		right : '3%',
+		left : '3%',
+		color : '#232226',
+		font : {
+			fontFamily : 'Helvetica Neue',
+			fontSize : '13dp',
+			fontWeight : 'normal',
+		},
+		text : commentText,
+		// textAlign : 'left',
+	});
+	row.add(text);
+	tableData.push(row);
+
+	var row = createPlainRow('auto');
+	var date = Ti.UI.createLabel({
+		width : '94%',
+		right : '3%',
+		left : '3%',
+		color : '#48464e',
+		font : {
+			fontFamily : 'Helvetica Neue',
+			fontSize : '8dp',
+			fontWeight : 'normal',
+		},
+		text : commentDate,
+		// textAlign : 'left',
+	});
+	row.add(date);
+	tableData.push(row);
+}
+
+function displayComments() {
+	// display the top 2 comments first and rest of them,
+	// should be hidden with the 'see more comments' text
+	// once that text is clicked it should load all the comments
+
+	var comments = [{
+		"id" : "1",
+		"body" : "Testing 1",
+		"date" : "2014-06-26 15:47:17"
+	}, {
+		"id" : "2",
+		"body" : "Testing 2",
+		"date" : "2014-06-26 15:47:18"
+	}, {
+		"id" : "3",
+		"body" : "Testing 3",
+		"date" : "2014-06-26 15:47:19"
+	}, {
+		"id" : "4",
+		"body" : "Testing 4",
+		"date" : "2014-06-26 15:47:20"
+	}];
+
+	creatingCommentTextHeading();
+	var commentsLengthLimit = 2;
+	var commentsLength = (comments.length > commentsLengthLimit) ? commentsLengthLimit : comments.length;
+	for (var i = 0; i < commentsLength; i++) {
+		addCommentToView(comments[i].body, comments[i].date);
+	}
+
+	if (comments.length > commentsLengthLimit) {
+		var row = createPlainRow('auto');
+		var text = Ti.UI.createLabel({
+			top : 10,
+			width : '94%',
+			right : '3%',
+			left : '3%',
+			color : '#005ab3',
+			font : {
+				fontFamily : 'Helvetica Neue',
+				fontSize : '13dp',
+				fontWeight : 'normal',
+			},
+			text : "Show more comments",
+			textAlign : 'center',
+		});
+		row.add(text);
+
+		// if clicked, hide it and show the other comments
+		row.addEventListener('click', function(e) {
+			// row.remove(text);
+			// text.visible = false;
+			tableData.pop();
+			// remove the last element, which is the "show more comments" row in this case
+			for (var i = 2; i < comments.length; i++) {
+				addCommentToView(comments[i].body, comments[i].date);
+			}
+			addTableDataToTheView();
+		});
+
+		tableData.push(row);
+
+	}
+
+}
+
 function initializePage() {
 	changeTitleOfThePage(post_content.name);
-	
+
 	if (post_content.parts) {
 		for (var i = 0; i < post_content.parts.length; i++) {
 			Ti.API.info(post_content.parts[i].type);
@@ -197,7 +320,8 @@ function initializePage() {
 			}
 
 			if (post_content.parts[i].type == "video") {
-				displayVideo(/*post_content.parts[i].image*/ post_content.image /*thumbnail*/, post_content.parts[i].video/*video*/);
+				displayVideo(/*post_content.parts[i].image*/
+				post_content.image/*thumbnail*/, post_content.parts[i].video/*video*/);
 			}
 
 			if (i === 0) {
@@ -205,8 +329,14 @@ function initializePage() {
 			}
 
 		}
-		addTableDataToTheView();
 	}
+
+	// if(post_content.comments){
+	displayComments();
+	// }
+
+	addTableDataToTheView();
+
 }
 
 /*
