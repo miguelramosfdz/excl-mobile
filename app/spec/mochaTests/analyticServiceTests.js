9d5a3e5094e3451871fa6a07a830fa7b83afea48
Sean Fisher <sean.fisher@parivedasolutions.com>
2014-06-27 11:17:24 -0500

    Analytics test stubs

---
+++ app/spec/mochaTests/analyticServiceTests.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+var sinon = require("sinon");
+
+var analyticsController = require('../../lib/analyticService/analyticService');
+
+describe('Analytics Service', function(){
+	
+});
