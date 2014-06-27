002243c897688b3cb28120bf79f05b3304072124
Kyle Clark <kyle.clark@parivedasolutions.com>
2014-06-27 13:10:35 -0500

    it's making me do this again. not sure why

--- app/spec/mochaTests/flyoutTests.js
+++ app/spec/mochaTests/flyoutTests.js
@@ -1,44 +1,115 @@
 var flyoutService = require('../../lib/navigationService/flyoutService');
-var navController = require('../../lib/navigationService/navigationService');
+var assert = require("assert");
 var sinon = require("sinon");
 
 describe('Flyout Menu', function(){
-	describe('#indexOf()', function(){
-		it('should return false when kiosk mode is on', function(){
-			//
-			assertTrue(true);
+	var menu;
+	var getOpenAnimationStub;
+	var getCloseAnimationStub;
+	var getMenuStub;
+	
+	before(function(){			
+		menu = {
+			animate: function(){}
+		};
+		getOpenAnimationStub = sinon.stub(flyoutService, 'getOpenAnimation');
+		getCloseAnimationStub = sinon.stub(flyoutService, 'getCloseAnimation');
+		getMenuStub = sinon.stub(flyoutService, 'getMenu').returns(menu);
+	});
+	
+	describe('closeMenu', function(){
+		it('should return false if operated on when kiosk mode is on', function(){	
+			var stub = sinon.stub(flyoutService, 'isEnabled').returns(false);		
+			assert(!flyoutService.closeMenu(), "The menu returned true when asked to close");
+			stub.restore();
+		});	
+	});
+	describe('openMenu', function(){
+		it('should return false if operated on when kiosk mode is on', function(){	
+			var stub = sinon.stub(flyoutService, 'isEnabled').returns(false);		
+			assert(!flyoutService.openMenu(), "The menu returned true when asked to open");
+			stub.restore();
+		});	
+		it('should be called when toggleMenu ', function(){	
+			var stub = sinon.stub(flyoutService, 'isEnabled').returns(false);		
+			assert(!flyoutService.openMenu(), "The menu returned true when asked to open");
+			stub.restore();
+		});			
+	});
+	describe('toggleMenu', function(){
+		it('should return false if operated on when kiosk mode is on', function(){	
+			var stub = sinon.stub(flyoutService, 'isEnabled').returns(false);		
+			assert(!flyoutService.toggleMenu(), "The toggle returned true");
+			stub.restore();
 		});
-		it('should return false when kiosk mode is off and the menu is visible', function(){
-			//
-			assertTrue(true);
+	});
+	describe('getOpenAnimation',function(){
+		it('should be called when openMenu is called and kiosk mode is disabled', function(){			
+			var stub = sinon.stub(flyoutService, 'isEnabled').returns(true);	
+			flyoutService.openMenu();
+			assert(getOpenAnimationStub.called, 'was not called when openMenu was called');	
+			stub.restore();	
 		});
-		it('should return true when kiosk mode is off and the menu is not visible', function(){
-			//
-			assertTrue(true);
+		
+	});	
+	
+	describe('getCloseAnimation',function(){
+		it('should be called when closeMenu is called', function(){			
+			var stub = sinon.stub(flyoutService, 'isEnabled').returns(true);	
+			flyoutService.closeMenu();
+			assert(getCloseAnimationStub.called, 'was not called when closeMenu was called');
+			stub.restore();	
 		});
+	});
+	
+	describe('isEnabled',function(){
+		it('should be called when openMenu is called', function(){			
+			var stub = sinon.stub(flyoutService, 'isEnabled');
+			flyoutService.openMenu();
+			assert(stub.called, 'was not called when openMenu was called');
+			stub.restore();	
+		});
+	});
+	
+	describe('getMenu', function(){
+		beforeEach(function(){	
+			getMenuStub.restore();		
+			menu = {
+				animate: function(){}
+			};
+
+			//getOpenAnimationStub = sinon.stub(flyoutService, 'getOpenAnimation');
+			//getCloseAnimationStub = sinon.stub(flyoutService, 'getCloseAnimation');
+			getMenuStub = sinon.stub(flyoutService, 'getMenu').returns(menu);
+		});
+		
+		it('should be called when openMenu is called and Kiosk mode is disabled',function(){
+			var stub = sinon.stub(flyoutService, 'isEnabled').returns(true);	
+			flyoutService.openMenu();
+			assert(getMenuStub.called, 'was not called when Kiosk mode was disabled');
+			stub.restore();		
+		});		
+		it('should not be called when openMenu is called and Kiosk mode is enabled',function(){
+			var stub = sinon.stub(flyoutService, 'isEnabled').returns(false);	
+			flyoutService.openMenu();
+			assert(!getMenuStub.called, 'was called when Kiosk mode was enabled');
+			stub.restore();	
+		});
+		it('should be called when closedMenu is called',function(){
+			flyoutService.closeMenu();
+			assert(getMenuStub.called, 'was not called');
+		});
+		it('should be called when toggleMenu is called and Kiosk mode is disabled',function(){
+			var stub = sinon.stub(flyoutService, 'isEnabled').returns(true);	
+			flyoutService.openMenu();
+			assert(getMenuStub.called, 'was not called');
+			stub.restore();	
+		});
+	});
+	
+	after(function(){
+		getOpenAnimationStub.restore();
+		getCloseAnimationStub.restore();
+		getMenuStub.restore();
 	});
 });
-
-/*
-describe('parsedJson', function(){
-	describe("parse", function() {
-		it("should return 'parsed' json data", function() {
-
-			var dataToSend = "\"social-media-settings\": {\"liking\": true,\"sharing\": false,\"commenting\": true},";
-			var expectedData = "\"social-media-settings\": {\"liking\":expectedData true,\"sharing\": false,\"commenting\": true},";
-			// spyOn(parseCalls, 'parse').andReturn(expectedData);
-			// var returnedData = dataRetriever.parseJson(dataToSend);
-			// expect(returnedData).toBe(expectedData);
-
-			// var spy = sinon.spy(parseCalls, "parse");
-			// dataRetriever.parseJson(dataToSend);
-			// parseCalls.parse(dataToSend);
-			// assertTrue(spy.called);
-
-			// var mock = sinon.mock(parseCalls);
-			// mock.expects('parse').once().returns(expectedData);
-			// mock.verify();
-
-		});
-	});
-});*/
