e703b8ff90b68a675b7a0f24cb63482d44732ccb
Sean Fisher <sean.fisher@parivedasolutions.com>
2014-07-01 08:44:46 -0500

    Added Google Analytics tracking to the Navigation controller. Known issue: right now it double counts hits for some reason.

--- app/lib/analyticService/analyticService.js
+++ app/lib/analyticService/analyticService.js
@@ -1,8 +1,15 @@
 
-function AnalyticsController() {}
+function AnalyticsController() {
+	this.pageLevelCustomDimensionIndex = 4; // Index from Google Analytics website // TODO: get from Wordpress dynamically
+}
 
 AnalyticsController.prototype.getTracker = function() {
+<<<<<<< HEAD
 	if (this.trackerID == null) {
+=======
+	if (!this.validateTrackerID(this.trackerID)) {
+		apiCalls.info("Invalid or no Google Analytics Tracker ID found. Turning off analytics.");
+>>>>>>> Added Google Analytics tracking to the Navigation controller. Known issue: right now it double counts hits for some reason.
 		return false;
 	}
 	if (this.tracker == null && this.trackerID != null) {
@@ -14,15 +21,27 @@ AnalyticsController.prototype.getTracker = function() {
 	return this.tracker;
 };
 
+AnalyticsController.prototype.validateTrackerID = function(trackerID) {
+	return /(UA|YT|MO)-\d+-\d+/i.test(trackerID);
+};
+
 AnalyticsController.prototype.setTrackerID = function(trackerID) {
 	this.trackerID = trackerID;
 };
 
-AnalyticsController.prototype.trackScreen = function(screenName){
+AnalyticsController.prototype.trackScreen = function(screenName, pageLevel){
 	var tracker = this.getTracker();
 	if (!tracker) {return false;}
-	Ti.API.info("Now tracking screen " + screenName);
-	tracker.trackScreen(screenName);
+
+	var customDimensionObject = {};
+	customDimensionObject[this.pageLevelCustomDimensionIndex] = pageLevel;
+
+	apiCalls.info("Now tracking screen " + screenName);
+	var properties = {
+		path: screenName,
+		customDimension: customDimensionObject
+	};
+	tracker.trackScreen(properties);
 };
 
 AnalyticsController.prototype.trackEvent = function(category, action, label, value) {
