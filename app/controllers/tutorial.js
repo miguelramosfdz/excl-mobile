866cd89fc1ccf8b54d6dc5288177596a4266d809
Sean Fisher <sean.fisher@parivedasolutions.com>
2014-07-09 15:34:01 -0500

    Started to integrate tutorial main screen

---
+++ app/controllers/tutorial.js
@@ -0,0 +1,8 @@
+var args = arguments[0] || {};
+
+function tutorialGotIt() {
+	// var newView = Ti.UI.createView();
+	// $.overlayView.add(newView);
+	// $.addClass(newView, "fullScreenOverlay");
+	$.getView().close();
+}
