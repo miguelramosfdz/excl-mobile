5da78abf41ade15411e3ab1a657edae012177804
Sean Fisher <seandavidfisher@gmail.com>
2014-07-14 14:53:51 -0500

    Updates to tutorial service

--- app/lib/tutorialService/tutorialService.js
+++ app/lib/tutorialService/tutorialService.js
@@ -29,37 +29,36 @@ TutorialService.prototype.setNavService = function(navService) {
 };
 
 TutorialService.prototype.initializePagesToShowTutorial = function() {
-	var storage = this.getStorageService();
-	var pages = ["exhibitLandingTutorial", "componentLandingTutorial", "postLandingTutorial"];
-	for (var i = 0; i < pages.length; i++) {
-		if (!storage.getProperty(pages[i])) {
-			storage.setProperty(pages[i], true);
-		}
-	}
+	this.setAllPagesTo(true);
 };
 
 TutorialService.prototype.endTutorialMode = function() {
+	this.setAllPagesTo(false);
+};
+
+TutorialService.prototype.setAllPagesTo = function(trueOrFalse) {
 	var storage = this.getStorageService();
 	var pages = ["exhibitLandingTutorial", "componentLandingTutorial", "postLandingTutorial"];
 	for (var i = 0; i < pages.length; i++) {
-		if (!storage.getProperty(pages[i])) {
-			storage.setProperty(pages[i], false);
-		}
+		storage.setProperty(pages[i], trueOrFalse);
 	}
 };
 
-String.prototype.lcfirst = function()
-{
-    return this.charAt(0).toLowerCase() + this.substr(1);
-};
-
 TutorialService.prototype.checkTutorialForPage = function(page) {
-	var pageName = page.analyticsPageLevel.replace(/\s/g, '') + "Tutorial";
-	pageName = pageName.lcfirst();
-	Ti.API.info("checking for page " + pageName);
+	var pageName = this.getPageNameFromWindow(page);
 	var storage = this.getStorageService();
 	var showTutorial = storage.getProperty(pageName);
 	return (showTutorial) ? pageName : false;
+};
+
+TutorialService.prototype.gotIt = function(page) {
+	var storage = this.getStorageService();
+	storage.setProperty(page, false);
+};
+
+TutorialService.prototype.getPageNameFromWindow = function(page) {
+	var pageName = page.analyticsPageLevel.replace(/\s/g, '') + "Tutorial";
+	return pageName.charAt(0).toLowerCase() + pageName.substr(1);
 };
 
 module.exports = TutorialService;
