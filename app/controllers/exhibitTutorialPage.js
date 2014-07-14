46bef91a3206f297f96d91f5c5d9f7809996ced1
Harry Zhang <harry.zhang@parivedasolutions.com>
2014-07-14 11:12:48 -0500

    Tutorial View Close enabled

--- app/controllers/exhibitTutorialPage.js
+++ app/controllers/exhibitTutorialPage.js
@@ -4,7 +4,13 @@ function tutorialExit() {
 	// var newView = Ti.UI.createView();
 	// $.overlayView.add(newView);
 	// $.addClass(newView, "fullScreenOverlay");
+	$.exhibitTutorialPage.hide();
+	//$.exhibitTutorialPage.remove(view);
 	Alloy.Globals.navController.home();
+}
+
+function init(){
+	//$.ExitButton.addEventListener("click", );
 }
 
 function closeMenu(e) {
