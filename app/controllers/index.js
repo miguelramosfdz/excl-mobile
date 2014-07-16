c1b9937edadf11aad57b7e3017a7a4aa0b74e465
Dan Hogue <chmod740@gmail.com>
2014-07-16 10:00:19 -0500

    add map and info pages

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,21 @@
 var args = arguments[0] || {};
 
 
-var controller = Alloy.createController("exhibitLanding");
-Alloy.Globals.navController.open(controller);
+
+function openExhibits() {
+	var controller = Alloy.createController("exhibitLanding");
+	Alloy.Globals.navController.open(controller);	
+}
+
+function openMap() {
+	var controller = Alloy.createController("map");
+	Alloy.Globals.navController.open(controller);	
+}
+
+function openInfo() {
+	var controller = Alloy.createController("info");
+	Alloy.Globals.navController.open(controller);	
+}
+
+
+Alloy.Globals.navController.open(this);
