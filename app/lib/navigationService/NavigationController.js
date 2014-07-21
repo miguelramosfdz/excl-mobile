351ddbeffb2d9c5922e8725f0dcec3bb69f53a3c
Kyle Clark <kyle.clark@parivedasolutions.com>
2014-07-21 17:00:17 -0500

    got rid of home button navigation bug

--- app/lib/navigationService/NavigationController.js
+++ app/lib/navigationService/NavigationController.js
@@ -14,16 +14,8 @@ function NavigationController() {
 	this.tutorialController = new TutorialController();
 }
 
-NavigationController.prototype.restart = function(){
-		
-		if (this.windowStack && this.windowStack > 0) {
-			var lastWindow = this.windowStack.pop();
-			if (lastWindow) {
-				lastWindow.close();
-			}
-		}
-		Alloy.createController("index");
-		this.home();
+NavigationController.prototype.restart = function(){	
+	this.home();
 };
 
 NavigationController.prototype.enterKioskMode = function(){
