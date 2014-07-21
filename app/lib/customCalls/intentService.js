ecd1365afe5e8d8ffe2b07c069ddfec4f5ea1aba
Jennifer Long <jennifer.long@parivedasolutions.com>
2014-07-21 17:20:45 -0500

    Restore prepopulated text on iOS image sharing

--- app/lib/customCalls/intentService.js
+++ app/lib/customCalls/intentService.js
@@ -39,13 +39,27 @@ intentService.prototype.sendIntentImageAndroid = function(postTags, imageFilePat
 intentService.prototype.sendIntentImageiOS = function(postTags, imageFilePath, imageFilePathInstagram, instagramAnchor){
 	//Use TiSocial.Framework module to send image to other apps
 
+	var Social = require('dk.napp.social');
+	if (Social.isActivityViewSupported()) {
+		Social.activityView({
+			image : imageFilePath,
+			text : postTags
+		}, [{
+			title : "Instagram",
+			type : "open.instagram",
+			image : "/images/instagram-256.png",
+			callback : function(e) {
+				this.openInstagram(imageFilePathInstagram, instagramAnchor);
+			}}]);
+
+	} else {
+		alert("Photo sharing is not available on this device");
+	}
+};
+
+intentService.prototype.openInstagram = function(imageFilePathInstagram, instagramAnchor){
 	var docViewer = Ti.UI.iOS.createDocumentViewer({ "url": imageFilePathInstagram });
 	docViewer.UTI = "com.instagram.exclusivegram";
-	/*
-	var annotationObj = new Object();
-	annotationObj.InstagramCaption = postTags;
-	docViewer.annotation = annotationObj;
-	*/
 	docViewer.show({ "view": instagramAnchor, "animated": true });
 };
 
