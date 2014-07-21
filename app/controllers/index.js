b03c068d776a205f4a6b1acc52ddbfb13cad21fb
Alex Pearce <alex.pearce@parivedasolutions.com>
2014-07-21 15:52:38 -0500

    cleaned up index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,6 +2,18 @@ var args = arguments[0] || {};
 
 var analyticsPageTitle = "Home";
 var analyticsPageLevel = "Home";
+var url = Alloy.Globals.rootWebServiceUrl;
+var dataRetriever = setPathForLibDirectory('dataRetriever/dataRetriever');
+
+function setPathForLibDirectory(libFile) {
+	if ( typeof Titanium == 'undefined') {
+		lib = require("../../lib/" + libFile);
+	} else {
+		lib = require(libFile);
+	}
+	return lib;
+};
+
 function setAnalyticsPageTitle(title) {
 	analyticsPageTitle = title;
 }
@@ -39,11 +51,6 @@ function openInfo() {
 	Alloy.Globals.navController.open(controller);	
 }
 
-var url = Alloy.Globals.rootWebServiceUrl;
-
-//var dataRetriever = setPathForLibDirectory('dataRetriever/dataRetriever');
-var dataRetriever = require('dataRetriever/dataRetriever');
-
 function setPathForLibDirectory(libFile) {
 	if ( typeof Titanium == 'undefined') {
 		lib = require("../../lib/" + libFile);
@@ -52,17 +59,16 @@ function setPathForLibDirectory(libFile) {
 	}
 }
 
-
-function initializeWithJSON(json, controller) {
-	populateWindow(json);
-	// Alloy.Globals.navController.open(controller);
-}
-
-
 function retrieveJson(jsonURL, controller) {
 	dataRetriever.fetchDataFromUrl(jsonURL, function(returnedData) {
 		if (returnedData) {
-			initializeWithJSON(returnedData, controller);
+			var museums = Alloy.Collections.instance('museum');
+	var museumModel = Alloy.createModel('museum');
+	var page_info = json.data.museum.info;
+			 museumModel.set({
+				 'info' : museum.info,
+			 });
+			 museums.add(museumModel);
 		}
 	});
 }
@@ -71,17 +77,8 @@ function reloadWithJSON(json, controller) {
 	populateWindow(json);
 }
 
-function populateWindow(json) {
-	var museums = Alloy.Collections.instance('museum');
-	var museumModel = Alloy.createModel('museum');
-	var page_info = json.data.museum.info;
-			 museumModel.set({
-				 'info' : museum.info,
-			 });
-			 museums.add(museumModel);
-	
 	//Ti.API.info("******************************************TESTING***************************************************");
-}
+
 
 retrieveJson(url, this);
 
